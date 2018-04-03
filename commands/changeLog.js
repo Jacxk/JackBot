314b1c5b6ee52f860b767c1e9bafa2a4f30b56b5
Jack <tfermin1827@hotmail.com>
2018-04-02 22:04:43 -0400

    Fixed a typo on changeLog command

--- commands/changeLog.js
+++ commands/changeLog.js
@@ -7,7 +7,7 @@ module.exports.run = (message, args) => {
 
         files.forEach(file => {
             versions.set(file.filename, file);
-            fs.readFile(`./changelog/${file}`, 'utf8', (err, f) => {
+            fs.readFile(`./changeLog/${file}`, 'utf8', (err, f) => {
                 if (err) return console.error(err);
                 versions.set(file, f);
             });
@@ -15,7 +15,7 @@ module.exports.run = (message, args) => {
 
         if (args.length === 1) return message.channel.send(versions.get("v1.0"));
 
-        message.channel.send(versions.get(args[1])).catch(err => console.log(err.stack))
+        message.channel.send(versions.get(args[1])).catch(err => message.channel.send(err.toString()))
     });
 };
 
