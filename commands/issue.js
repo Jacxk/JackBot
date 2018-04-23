527f9c488e1bf0c9303d5a6dafe9b4c96818882f
Jack <tfermin1827@hotmail.com>
2018-04-23 19:18:37 -0400

    Fixed the issue command not giving the guild's invite link

--- commands/issue.js
+++ commands/issue.js
@@ -2,15 +2,19 @@ const Discord = require('discord.js');
 const messageUtil = require('../utilities/messageUtil.js');
 
 module.exports.run = (message, args, nothing, bot) => {
+    message.delete().catch(err => console.log(err.toString()));
+
     if (args.length < 2)
         return messageUtil.wrongUsage(message.channel, 'issue [message]', 'issue The guildInfo command is not working');
 
     const issue = args.slice(1).join(' ');
     const embed = new Discord.RichEmbed();
 
-    embed.setTitle('New Issue').setColor('GREEN').setDescription(issue).addField('Guild Name', message.guild.name)
-        .addField('User', message.member.displayName).addField('Date', message.createdAt.toDateString());
-    message.channel.createInvite().then(invite => embed.setURL(invite.url)).catch(err => console.error(err.toString()));
+    embed.setColor('GREEN').setDescription(issue).addField('Guild Name', message.guild.name)
+        .addField('User', message.member.displayName).addField('Date', message.createdAt.toDateString())
+        .setThumbnail(message.guild.iconURL);
+    message.channel.createInvite().then(invite => embed.setAuthor('New Issue', message.author.avatarURL, invite.url))
+        .catch(err => console.error(err.toString()));
 
     const channel = bot.channels.get('434434213148884992');
 
