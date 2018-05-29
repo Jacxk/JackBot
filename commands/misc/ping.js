299c64bdb2f2ffdc33ced1189368a7526eb3c66b
Jack <tfermin1827@hotmail.com>
2018-05-28 20:12:25 -0400

    Added a ping command to get the response time and the latency of the bot

---
+++ commands/misc/ping.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+module.exports.run = (message, args, a, bot) => {
+    let embed = new Discord.RichEmbed().setColor("GOLD");
+    const time = Date.now();
+
+    embed.setAuthor(`PONG!`, bot.user.displayAvatarURL);
+    embed.addField('My Ping', '🔌 ' + Math.round(bot.ping) + 'ms', true);
+
+    embed.setTimestamp();
+
+    message.channel.send('PING?!?!').then(msg => {
+        embed.addField('Response Time', '🔌 ' + Math.round(Date.now() - time) + 'ms', true);
+        msg.edit(embed).catch(err => console.log(err))
+    });
+};
+
+module.exports.command = {
+    name: 'ping',
+    permission: "none",
+    description: "PONG!! Wanna check my ping and response time??",
+    enabled: true
+};
